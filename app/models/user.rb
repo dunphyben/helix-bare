9701d3afb008d0953ab7d58aeb8c70cb162d326c
Benjamin Paul Dunphy & Maureen Dugan <student@epicodus.com>
2014-04-04 10:59:21 -0700

    add validation for user.name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :ideas
   validates :name, :uniqueness => true, :presence => true
 
+  validates :name, length: {
+    minimum: 4,
+    maximum: 17,
+    too_short: "Must have at least 4 letters",
+    too_long: "Must have fewer than 17 characters"
+  }
+
+  validates :name, format: {
+    :with => /\A[a-zA-Z0-9]+\z/,
+    :message => "Please use only regular letters as username"
+  }
+
+
 end
+
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,12 @@ describe User do
   it { should validate_uniqueness_of :name }
   it { should validate_presence_of :name }
 
+  it { should allow_value('Ben123').for(:name) }
+  it { should_not allow_value('Moe Rocca').for(:name)}
+
+  it { should_not allow_value('123').for(:name) }
+  it { should_not allow_value('123fmiumkfumumkuftkmifmki7ri87676').for(:name) }
+  it { should_not allow_value('sdflafj53%').for(:name) }
+
+
 end
